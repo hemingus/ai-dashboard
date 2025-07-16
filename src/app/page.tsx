8ed9aae88fedead0ba6a7192923a5e7b7c138b53
Heming Hanevik <heming.hanevik@gmail.com>
2025-07-16 23:59:25 +0200

    added header to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div className={styles.page}>
+      <h1 className={styles.header}>Ask the AI</h1>
       <OpenAItest />
     </div>
   );
